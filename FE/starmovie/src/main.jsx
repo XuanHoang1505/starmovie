6f38d68d547c8480e7f0c9658c94abc5b10384d6
linhnguyen2209 <linhli2k4@gmail.com>
2025-03-20 09:22:08 +0700

    add library

--- FE/starmovie/src/main.jsx
+++ FE/starmovie/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import GlobalStyles from "./common/GlobalStyles";
+import { HelmetProvider } from "react-helmet-async";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GlobalStyles>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </GlobalStyles>
+  </StrictMode>
+);
