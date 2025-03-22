d28661648018e9d09dc4bcd15bb6e69c1032a535
linhnguyen2209 <linhli2k4@gmail.com>
2025-03-22 16:36:01 +0700

    Custom project

--- FE/starmovie/src/main.jsx
+++ FE/starmovie/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import GlobalStyles from "./common/GlobalStyles";
 import { HelmetProvider } from "react-helmet-async";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -10,10 +9,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalStyles>
       <HelmetProvider>
         <App />
       </HelmetProvider>
-    </GlobalStyles>
   </StrictMode>
 );
